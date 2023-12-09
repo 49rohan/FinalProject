a404d3909c5a640ce78b7044f3c38e1023261951
Rohan Basnet <rohan.basnet@southeasttech.edu>
2023-12-08 18:32:54 -0600

    Revert "Changes:"
    This reverts commit 10d45b8200465b7fadd29dd638beb513b2cde25a.

--- FinalProject/Controllers/HomeController.cs
+++ FinalProject/Controllers/HomeController.cs
@@ -1,27 +1,20 @@
 ﻿using FinalProject.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Linq;
+using System.Diagnostics;
 
 namespace FinalProject.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly LeagueDbContext _context;
-
-        //Constructor 
-        public HomeController(LeagueDbContext context)
-        {
-            _context = context;
-        }
-
-        //Action for index view
         public IActionResult Index()
         {
-            //Get the list of champions from the database
-            var champions = _context.Champs.ToList();
-
-            //Pass champions to index view
-            return View(champions);
+            return View();
         }
+/*
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+*/
     }
 }
